d58e24e081f317d7427fb34cd622b6368c407c9d
Sebastien Berube <sebastienb@momentfactory.com>
2022-12-30 19:12:08 -0500

    Remove path from CompileShader batch file

--- data/shaders/glsl/raytracingshadows/CompileShaders.bat
+++ data/shaders/glsl/raytracingshadows/CompileShaders.bat
@@ -1,9 +1,9 @@
 @echo off
 @echo:
 @echo Compiling shaders...
-C:\VulkanSDK\1.2.198.1\Bin\glslangValidator.exe -V C:\Dev\Perso\GitHub\VulkanExamples\data\shaders\glsl\raytracingshadows\miss.rmiss       -o C:\Dev\Perso\GitHub\VulkanExamples\data\shaders\glsl\raytracingshadows\miss.rmiss.spv       --target-env spirv1.4
-C:\VulkanSDK\1.2.198.1\Bin\glslangValidator.exe -V C:\Dev\Perso\GitHub\VulkanExamples\data\shaders\glsl\raytracingshadows\closesthit.rchit -o C:\Dev\Perso\GitHub\VulkanExamples\data\shaders\glsl\raytracingshadows\closesthit.rchit.spv --target-env spirv1.4
-C:\VulkanSDK\1.2.198.1\Bin\glslangValidator.exe -V C:\Dev\Perso\GitHub\VulkanExamples\data\shaders\glsl\raytracingshadows\raygen.rgen      -o C:\Dev\Perso\GitHub\VulkanExamples\data\shaders\glsl\raytracingshadows\raygen.rgen.spv      --target-env spirv1.4
-C:\VulkanSDK\1.2.198.1\Bin\glslangValidator.exe -V C:\Dev\Perso\GitHub\VulkanExamples\data\shaders\glsl\raytracingshadows\shadow.rmiss     -o C:\Dev\Perso\GitHub\VulkanExamples\data\shaders\glsl\raytracingshadows\shadow.rmiss.spv     --target-env spirv1.4
+glslangValidator.exe -V miss.rmiss       -o miss.rmiss.spv       --target-env spirv1.4
+glslangValidator.exe -V closesthit.rchit -o closesthit.rchit.spv --target-env spirv1.4
+glslangValidator.exe -V raygen.rgen      -o raygen.rgen.spv      --target-env spirv1.4
+glslangValidator.exe -V shadow.rmiss     -o shadow.rmiss.spv     --target-env spirv1.4
 @echo:
 pause
