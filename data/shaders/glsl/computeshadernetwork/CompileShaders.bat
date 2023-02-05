dd94883dc91d5435788bb3f4fd4ce8321b1162eb
Sebastien Berube <sebastienb@momentfactory.com>
2023-02-04 22:44:22 -0500

    Shader update : Add channelswap, clean-up others

--- data/shaders/glsl/computeshadernetwork/CompileShaders.bat
+++ data/shaders/glsl/computeshadernetwork/CompileShaders.bat
@@ -1,12 +1,13 @@
 @echo off
 @echo:
 @echo Compiling shaders...
-glslangValidator.exe -V edgedetect.comp -o edgedetect.comp.spv
-glslangValidator.exe -V emboss.comp     -o emboss.comp.spv
-glslangValidator.exe -V sharpen.comp    -o sharpen.comp.spv
-glslangValidator.exe -V threshold.comp  -o threshold.comp.spv
-glslangValidator.exe -V blur.comp       -o blur.comp.spv
-glslangValidator.exe -V texture.frag    -o texture.frag.spv
-glslangValidator.exe -V texture.vert    -o texture.vert.spv
+glslangValidator.exe -V edgedetect.comp  -o edgedetect.comp.spv
+glslangValidator.exe -V emboss.comp      -o emboss.comp.spv
+glslangValidator.exe -V sharpen.comp     -o sharpen.comp.spv
+glslangValidator.exe -V threshold.comp   -o threshold.comp.spv
+glslangValidator.exe -V blur.comp        -o blur.comp.spv
+glslangValidator.exe -V channelswap.comp -o channelswap.comp.spv
+glslangValidator.exe -V texture.frag     -o texture.frag.spv
+glslangValidator.exe -V texture.vert     -o texture.vert.spv
 @echo:
 pause
