3cb971c05d580151f5d110157c69365c947465d0
Sebastien Berube <berubeseb@gmail.com>
2023-07-21 15:13:48 -0400

    Add ASSERT(x) without message

--- base/AssertMsg.h
+++ base/AssertMsg.h
@@ -9,9 +9,11 @@
 #pragma once
 
 #ifdef NDEBUG
-#define ASSERT_MSG(condition) ((void)0)
+#define ASSERT_MSG(exp, msg) ((void)0)
+#define ASSERT(exp) ((void)0)
 #else
 #include <iostream>
 #include <assert.h>
 #define ASSERT_MSG(exp, msg) { if(!(exp)) { std::cerr << (msg); } assert(exp); }
+#define ASSERT(exp) assert(exp);
 #endif
